844d6d06d5358732e70a4d1f5e004dd8b86d85a9
gui machiavelli <hey@guimachiavelli.com>
2014-04-26 15:35:09 +0100

    removed global elements from index.php, added as template parts

--- content/themes/pitaveri/index.php
+++ content/themes/pitaveri/index.php
@@ -1,72 +1,33 @@
-<!doctype html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8 lt-ie10"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9 lt-ie10"> <![endif]-->
-<!--[if IE 9]>         <html class="no-js lt-ie10"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<?php get_header(); ?>
 
-		<title>Pitaveri</title>
-		<meta name="description" content="">
+	<article class="main-content">
 
-		<meta name="viewport" content="width=device-width">
+		<h1 class="project-title">Osservatorio didattica educazione 2013/02</h1>
+		<figure class="detail project-image featured-image">
+			<img src="<?php echo TEMPLATE_URL; ?>/placeholder/detail1.jpg" alt="osservatorio">
+		</figure>
 
-		<link rel="stylesheet" href="<?php echo TEMPLATE_URL; ?>/css/pitaveri-main.css">
-
-		<script src="<?php echo TEMPLATE_URL; ?>/js/libs/modernizr.js"></script>
-		<script data-main="<?php echo TEMPLATE_URL; ?>/js/pitaveri-main" src="<?php echo TEMPLATE_URL; ?>/js/libs/require.min.js"></script>
-	</head>
-
-	<body class="projects">
-
-		<div id="wrapper">
-
-			<nav id="primary-nav" class="white">
-				<h1 id="menu-title" class="outline active">Menu</h1>
-				<ul>
-					<li class="menu-item pippo"><a href="#">Filippo Taveri</a></li>
-					<li class="menu-item works"><a href="#">Works</a></li>
-					<li class="menu-item pippo"><a href="#">About</a></li>
-				</ul>
-			</nav>
-
-			<article class="main-content">
-
-				<h1 class="project-title">Osservatorio didattica educazione 2013/02</h1>
-				<figure class="detail project-image featured-image">
-					<img src="<?php echo TEMPLATE_URL; ?>/placeholder/detail1.jpg" alt="osservatorio">
-				</figure>
-
-				<div class="description">
-					<p>Basic <a class="test-link" href="#">bookbinding</a> manual focused on the use of recycled materials and makeshift tools. The publication has been written and designed after a series of workshops held towards the end of 2011 in Bari. Afterwards, we replicated the workshop around Italy, and actively improve the course and promote it in other venues.</p>
-				</div>
-
-				<figure class="regular  project-image">
-					<img src="<?php echo TEMPLATE_URL; ?>/placeholder/regular1.jpg" alt="osservatorio">
-				</figure>
-
-				<figure class="regular project-image">
-					<img src="<?php echo TEMPLATE_URL; ?>/placeholder/regular2.jpg" alt="osservatorio">
-				</figure>
-
-				<figure class="detail project-image">
-					<img src="<?php echo TEMPLATE_URL; ?>/placeholder/detail2.jpg" alt="osservatorio">
-				</figure>
-
-				<figure class="regular project-image">
-					<img src="<?php echo TEMPLATE_URL; ?>/placeholder/regular1.jpg" alt="osservatorio">
-				</figure>
-
-
-			</article>
-
-
-
+		<div class="description">
+			<p>Basic <a class="test-link" href="#">bookbinding</a> manual focused on the use of recycled materials and makeshift tools. The publication has been written and designed after a series of workshops held towards the end of 2011 in Bari. Afterwards, we replicated the workshop around Italy, and actively improve the course and promote it in other venues.</p>
 		</div>
 
+		<figure class="regular  project-image">
+			<img src="<?php echo TEMPLATE_URL; ?>/placeholder/regular1.jpg" alt="osservatorio">
+		</figure>
+
+		<figure class="regular project-image">
+			<img src="<?php echo TEMPLATE_URL; ?>/placeholder/regular2.jpg" alt="osservatorio">
+		</figure>
+
+		<figure class="detail project-image">
+			<img src="<?php echo TEMPLATE_URL; ?>/placeholder/detail2.jpg" alt="osservatorio">
+		</figure>
+
+		<figure class="regular project-image">
+			<img src="<?php echo TEMPLATE_URL; ?>/placeholder/regular1.jpg" alt="osservatorio">
+		</figure>
 
 
-	</body>
-</html>
+	</article>
+
+<?php get_footer(); ?>
---
+++ content/themes/pitaveri/nav.php
@@ -0,0 +1,9 @@
+<nav id="primary-nav" class="white">
+	<h1 id="menu-title" class="outline active">Menu</h1>
+	<ul>
+		<li class="menu-item pippo"><a href="<?php bloginfo('url'); ?>">Filippo Taveri</a></li>
+		<li class="menu-item works"><a href="#">Works</a></li>
+		<li class="menu-item about"><a href="#">About</a></li>
+	</ul>
+</nav>
+
