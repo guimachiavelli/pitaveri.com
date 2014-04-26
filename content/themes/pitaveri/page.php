0be1d241324ad8b12789099a6e46be59cfc049a7
gui machiavelli <hey@guimachiavelli.com>
2014-04-26 15:35:28 +0100

    added simple page template

---
+++ content/themes/pitaveri/page.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+	<article class="main-content">
+		<h1 class="page-title"><?php the_title(); ?></h1>
+		<main class="page-content">
+			<?php the_content(); ?>
+		</main>
+	</article>
+
+<?php get_footer(); ?>
