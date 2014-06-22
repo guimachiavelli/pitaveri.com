ef65c57010810915c6f8ab4072f555ed70d9d718
gui machiavelli <hey@guimachiavelli.com>
2014-06-23 00:48:30 +0100

    auto-activating plugins

--- content/themes/pitaveri/functions.php
+++ content/themes/pitaveri/functions.php
@@ -5,6 +5,7 @@
 	require_once TEMPLATE_DIR . '/includes/helpers.php';
 	require_once TEMPLATE_DIR . '/includes/images.php';
 	require_once TEMPLATE_DIR . '/includes/assets.php';
+	require_once TEMPLATE_DIR . '/includes/plugins.php';
 	require_once TEMPLATE_DIR . '/includes/media-uploader.php';
 	require_once TEMPLATE_DIR . '/includes/register-project.php';
 
