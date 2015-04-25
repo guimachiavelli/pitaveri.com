dc77067a0b3b8421852d0d35dad532f8ae5fb293
gui machiavelli <hey@guimachiavelli.com>
2015-04-25 20:35:22 +0100

    removed modernizr dependency

--- content/themes/pitaveri/includes/assets.php
+++ content/themes/pitaveri/includes/assets.php
@@ -4,19 +4,26 @@ add_action('wp_enqueue_scripts', 'p_enqueue_scripts');
 add_action('wp_enqueue_scripts', 'p_enqueue_styles');
 
 function p_enqueue_styles() {
-	wp_register_style('p-stylesheet', TEMPLATE_URL . '/css/pitaveri-main.css');
-	wp_enqueue_style('p-stylesheet');
+    wp_register_style('p-stylesheet', TEMPLATE_URL . '/css/pitaveri-main.css');
+    wp_enqueue_style('p-stylesheet');
 }
 
 function p_enqueue_scripts() {
     wp_deregister_script('jquery');
-	wp_register_script('modernizr', TEMPLATE_URL . '/vendor/modernizr/modernizr.js');
-	wp_register_script('jquery', 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js', false, false, true);
-	wp_register_script('parallax', TEMPLATE_URL . '/vendor/Parallax-ImageScroll/jquery.imageScroll.min.js',  array('jquery'), false, true);
-	wp_register_script('p-main', TEMPLATE_URL . '/js/pitaveri-main.js',  array('jquery'), false, true);
 
-	wp_enqueue_script('modernizr');
-	wp_enqueue_script('jquery');
-	wp_enqueue_script('parallax');
-	wp_enqueue_script('p-main');
+    wp_register_script('jquery',
+        'http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js',
+        false, false, true);
+
+    wp_register_script('parallax',
+        TEMPLATE_URL . '/vendor/Parallax-ImageScroll/jquery.imageScroll.min.js',
+        array('jquery'), false, true);
+
+    wp_register_script('p-main',
+        TEMPLATE_URL . '/js/pitaveri-main.js',
+        array('jquery'), false, true);
+
+    wp_enqueue_script('jquery');
+    wp_enqueue_script('parallax');
+    wp_enqueue_script('p-main');
 }
