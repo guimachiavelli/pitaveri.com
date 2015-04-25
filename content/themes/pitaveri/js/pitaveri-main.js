8825bfcbfe52f93fb84a4de772d6301a6be19fdd
gui machiavelli <hey@guimachiavelli.com>
2015-04-25 20:41:20 +0100

    added support for picturefill, manually checking for touch support

--- content/themes/pitaveri/js/pitaveri-main.js
+++ content/themes/pitaveri/js/pitaveri-main.js
@@ -1,71 +1,87 @@
-$.fn.isOnScreen = function(){
-	var $window, viewport = {}, bounds = {}, winHeight, elHeight;
+(function(){
+    /* global $ */
+    'use strict';
 
-	$window = $(window);
-	winHeight = $window.height();
-	elHeight = this.height();
+    $.fn.isOnScreen = function(){
+        var $window, viewport = {}, bounds = {}, winHeight, elHeight;
 
-	viewport.top = $window.scrollTop();
-	bounds.top = this.offset().top;
+        $window = $(window);
+        winHeight = $window.height();
+        elHeight = this.height();
 
-	viewport.bottom = viewport.top + winHeight - elHeight + 55;
-	bounds.bottom = bounds.top + elHeight - 45;
+        viewport.top = $window.scrollTop();
+        bounds.top = this.offset().top;
 
-	return !(viewport.bottom < bounds.top || viewport.top > bounds.bottom);
-};
+        viewport.bottom = viewport.top + winHeight - elHeight + 55;
+        bounds.bottom = bounds.top + elHeight - 45;
 
-var toggleMenuColor = function() {
-	if ($(document).scrollTop() < $('.detail').height() - 55) {
-		$('#primary-nav').addClass('white');
-		return;
-	}
+        return !(viewport.bottom < bounds.top || viewport.top > bounds.bottom);
+    };
 
-	$('.detail').each(function(){
-		if ($(this).isOnScreen()) {
-			$('#primary-nav').addClass('white');
-			return false;
-		} else if (!$(this).isOnScreen()) {
-			$('#primary-nav').removeClass('white');
-		}
-	});
-};
+    var toggleMenuColor = function() {
+        if ($(document).scrollTop() < $('.detail').height() - 55) {
+            $('#primary-nav').addClass('white');
+            return;
+        }
 
-var imageCover = function($detail_img) {
-	$detail_img.each(function(){
-		var $image = $(this).find('img'),
-			image_src = $image.attr('src');
-		//$(this).css('background-image', 'url("'+image_src+'")').find('img').remove();
-		$(this).attr('data-image', image_src).find('img').remove();
-	});
-	$detail_img.imageScroll({
-		coverRatio: 1,
-		speed: 0,
-		mediaWidth: 1400,
-		mediaHeight: 933
-	});
-};
+        $('.detail').each(function(){
+            if ($(this).isOnScreen()) {
+                $('#primary-nav').addClass('white');
+                return false;
+            } else if (!$(this).isOnScreen()) {
+                $('#primary-nav').removeClass('white');
+            }
+        });
+    };
 
-var projectListImage = 	function($detail_img) {
-	$detail_img.each(function(){
-		var $image = $(this).find('img'),
-			image_src = $image.attr('src');
-		$(this).css('background-image', 'url("'+image_src+'")').find('img').remove();
-	});
-};
+    var imageCover = function($detail_img) {
+        var $image, imageSrc, srcIndex;
+        $detail_img.each(function(){
+            $image = $(this).find('img');
+            imageSrc = $image.attr('srcset');
+
+            if (typeof imageSrc !== 'undefined') {
+                imageSrc = imageSrc.split(', ');
+                srcIndex = window.devicePixelRatio > 1.5 ? 1 : 0;
+                imageSrc = imageSrc[srcIndex].split(' ')[0];
+            } else {
+                imageSrc = $image.attr('src');
+            }
+
+            $(this).attr('data-image', imageSrc).find('img').remove();
+        });
+
+        $detail_img.imageScroll({
+            coverRatio: 1,
+            speed: 0,
+            mediaWidth: 1400,
+            mediaHeight: 933
+        });
+    };
+
+    var projectListImage = function($detail_img) {
+        $detail_img.each(function(){
+            var $image = $(this).find('img'),
+                image_src = $image.attr('src');
+            $(this).css('background-image', 'url("' + image_src + '")')
+                   .find('img')
+                   .remove();
+        });
+    };
 
 
-$(document).ready(function() {
-	if ($('html').hasClass('no-touch')) {
-		imageCover($('.detail'));
-		toggleMenuColor();
-		if ($('body').hasClass('single') || $('body').hasClass('home')) {
-			$(document).on('scroll', function(){
-				toggleMenuColor();
-			});
-		}
-	}
+    $(document).ready(function() {
+        if (!('ontouchstart' in window)) {
+            $('html').addClass('no-touch');
+            imageCover($('.detail'));
+            toggleMenuColor();
+            if ($('body').hasClass('single') || $('body').hasClass('home')) {
+                $(document).on('scroll', function(){
+                    toggleMenuColor();
+                });
+            }
+        }
 
-	projectListImage($('.project-list-image'));
-
-});
-
+        projectListImage($('.project-list-image'));
+    });
+}());
